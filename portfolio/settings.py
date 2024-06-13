3b73e97c0a1eb2b2a1af4c5640792b2d8e62bfed
Kelvin <kelvincedcas@gmail.com>
2024-06-13 14:47:26 -0500

    push14

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'portfolio/static'),)
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
